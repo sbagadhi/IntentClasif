1459967648bc7459258c2918747c519f4c1e7da2
Shalini <sbagadhi@3rdflix.com>
2020-03-20 16:20:12 +0530

    no message

--- query.py
+++ query.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
 
-import rasa
+
+
 from rasa.nlu.model import Interpreter
 
 
-# In[2]:
+
 
 
 interpreter=Interpreter.load('/users/sbagadhi/desktop/GIT/models/nlu/new')
 
 
-# In[3]:
+
 
 
 def ask_question(text):
     print(interpreter.parse(text))
 
 
-# In[ ]:
+while True:
+    query=input()
+    answer=ask_question(query)
+    print({"text":query,"intent":answer})
+    if query=="quit":
+        break
 
 
 
